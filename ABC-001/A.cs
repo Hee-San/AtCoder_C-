174c237534143413c9c58a568554a612b549aee5
へーさん <hira9603859504@gmail.com>
2017-02-23 15:40:51 +0900

    ABC-001-A 追加

---
+++ ABC-001/A.cs
@@ -0,0 +1,15 @@
+//A - êœê·ê[ç∑
+//http://abc001.contest.atcoder.jp/tasks/abc001_1
+
+
+using System;
+
+namespace ABC001A {
+	class Program {
+		public static void Main(){
+			int h1 = int.Parse(Console.ReadLine());
+			int h2 = int.Parse(Console.ReadLine());
+			Console.WriteLine(h1 - h2);
+		}
+	}
+}
