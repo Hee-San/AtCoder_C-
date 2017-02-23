b05790d12eabdf1b1e6897007cfd7dd09101ecf2
へーさん <hira9603859504@gmail.com>
2017-02-23 16:35:50 +0900

    ABC-001-B 追加

---
+++ ABC-001/B.cs
@@ -0,0 +1,39 @@
+// B - Ž‹’ö‚Ì’Ê•ñ
+// http://abc001.contest.atcoder.jp/tasks/abc001_2
+using System;
+
+namespace ABC001B {
+	class Program {
+		public Program() {}
+		
+		public static void Main() {
+			new Program().Calc();
+		}
+		
+		void Calc() {
+			var m = InputInt;	// m[m]
+			Console.WriteLine(getVV(m).ToString("D2"));
+		}
+		
+		int InputInt {
+			get {
+				return int.Parse(Console.ReadLine());
+			}
+		}
+		
+		int getVV(int m){
+			// SYNOP
+			if(m < 100)
+				return 0;
+			if(100 <= m && m <= 5000)
+				return m / 100;
+			if(6000 <= m && m <= 30000)
+				return (m / 1000) + 50;
+			if(35000 <= m && m <= 70000)
+				return ((m / 1000) - 30) / 5 + 80;
+			if(70000 < m)
+				return 89;
+			return -1;
+		}
+	}
+}
