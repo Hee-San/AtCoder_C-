8c76752c24bd97af3e29fa2ac6c8ab5b3b4c00a8
へーさん <hira9603859504@gmail.com>
2017-02-23 15:54:52 +0900

    リファクタリング

--- ABC-001/A.cs
+++ ABC-001/A.cs
@@ -1,15 +1,25 @@
 //A - êœê·ê[ç∑
 //http://abc001.contest.atcoder.jp/tasks/abc001_1
-
-
 using System;
 
 namespace ABC001A {
 	class Program {
-		public static void Main(){
-			int h1 = int.Parse(Console.ReadLine());
-			int h2 = int.Parse(Console.ReadLine());
+		public Program() {}
+		
+		public static void Main() {
+			new Program().Calc();
+		}
+		
+		void Calc() {
+			var h1 = InputInt;
+			var h2 = InputInt;
 			Console.WriteLine(h1 - h2);
+		}
+		
+		int InputInt {
+			get {
+				return int.Parse(Console.ReadLine());
+			}
 		}
 	}
 }
